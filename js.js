6771d618df68708b636626fa4ad83229c72c141f
Iuliia <yulia.lesikova@gmail.com>
2022-09-10 18:36:27 +0200

    3

--- js.js
+++ js.js
@@ -418,3 +418,21 @@ const myMusic = [{
         ]
     }
 ];
+
+
+// Setup
+var myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+};
+
+// Only change code below this line
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
