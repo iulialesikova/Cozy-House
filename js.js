b2c288e05d6fd0e872ffcf6886e4fdbbace1aab2
Iuliia <yulia.lesikova@gmail.com>
2022-09-10 18:38:02 +0200

    4

--- js.js
+++ js.js
@@ -436,3 +436,17 @@ var myStorage = {
 // Only change code below this line
 
 var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
+
+const myPlants = [{
+        type: "flowers",
+        list: ["rose", "tulip", "dandelion"]
+    },
+    {
+        type: "trees",
+        list: ["fir", "pine", "birch"]
+    }
+];
+
+// Only change code below this line
+
+const secondTree = myPlants[1].list[1];
