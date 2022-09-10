d0da2ced112cbbc9fd6699d3d22fe53f117f4db2
Iuliia <yulia.lesikova@gmail.com>
2022-09-10 15:41:02 +0200

    29

--- js.js
+++ js.js
@@ -164,6 +164,35 @@ console.log(increment(5)); // returns NaN
 
 const sum = (...args) => {
     return args.reduce((a, b) => a + b, 0);
-  }
+}
 
-  const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+const {
+    today: highToday,
+    tomorrow: highTomorrow
+} = HIGH_TEMPERATURES;
+
+const LOCAL_FORECAST = {
+    yesterday: {
+        low: 61,
+        high: 75
+    },
+    today: {
+        low: 64,
+        high: 77
+    },
+    tomorrow: {
+        low: 68,
+        high: 80
+    }
+};
+
+// Only change code below this line
+
+
+const {
+    today: {
+        low: lowToday,
+        high: highToday
+    }
+} = LOCAL_FORECAST;
+// Only change code above this line
