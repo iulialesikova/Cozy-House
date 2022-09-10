c8c3acefa37872d657f57a97fddee1ab6b5ddd9b
Iuliia <yulia.lesikova@gmail.com>
2022-09-10 15:31:32 +0200

    25

--- js.js
+++ js.js
@@ -147,3 +147,11 @@ function freezeObj() {
     return MATH_CONSTANTS.PI;
 }
 const PI = freezeObj();
+
+const magic = () => new Date();
+
+const myConcat = (arr1, arr2) =>  {
+    return arr1.concat(arr2);
+  };
+  
+  console.log(myConcat([1, 2], [3, 4, 5]));
