22eb3dfa676f6459dd84ae2c960c7d3c7e8a917b
Iuliia <yulia.lesikova@gmail.com>
2022-09-10 15:25:59 +0200

    23

--- js.js
+++ js.js
@@ -113,6 +113,19 @@ function booleanToString(b) {
     return b.toString();
 }
 
-function hexToDec(hexString){
-    return  parseInt(hexString, 16);
-   }
+function hexToDec(hexString) {
+    return parseInt(hexString, 16);
+}
+
+const s = [5, 7, 2];
+
+function editInPlace() {
+    "use strict";
+
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+
+}
+editInPlace();
+console.log(s);
