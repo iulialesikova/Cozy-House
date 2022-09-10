b68e6dfab0f5842d16dee3b33944d04ac1b35032
Iuliia <yulia.lesikova@gmail.com>
2022-09-10 18:01:02 +0200

    39

--- js.js
+++ js.js
@@ -330,3 +330,15 @@ cc(3);
 cc(7);
 cc('K');
 cc('A');
+
+
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  // Only change code below this line
+  myDog.name = "Happy Coder";
