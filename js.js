648e5731fb75ae00bb14e373207b2b0329e4065e
Iuliia <yulia.lesikova@gmail.com>
2022-09-10 11:01:43 +0200

    3

--- js.js
+++ js.js
@@ -2,3 +2,6 @@ squareOrSquareRoot = array => array.map(x => Math.sqrt(x) % 1 ? x * x : Math.sqr
 function getGrade (s1, s2, s3) {
     return {10:'A' ,9:'A' ,8:'B' ,7:'C' ,6:'D'} [Math.floor((s1 + s2 + s3) / 30)] || 'F'
     };
+function makeUpperCase(str) {
+        return str.toUpperCase()
+       }
