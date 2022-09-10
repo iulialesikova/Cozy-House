a581cb89add332d3d86282d9303bd44cebd9cff3
Iuliia <yulia.lesikova@gmail.com>
2022-09-10 10:43:21 +0200

    2

--- js.js
+++ js.js
@@ -1,1 +1,4 @@
-squareOrSquareRoot = array => array.map(x => Math.sqrt(x) % 1 ? x * x : Math.sqrt(x)) 
+squareOrSquareRoot = array => array.map(x => Math.sqrt(x) % 1 ? x * x : Math.sqrt(x)) ;
+function getGrade (s1, s2, s3) {
+    return {10:'A' ,9:'A' ,8:'B' ,7:'C' ,6:'D'} [Math.floor((s1 + s2 + s3) / 30)] || 'F'
+    };
