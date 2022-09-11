4838fdc9ca8b19fb57b1767f03b84fba8f4ccc97
Iuliia <yulia.lesikova@gmail.com>
2022-09-11 06:50:10 +0200

    9

--- js.js
+++ js.js
@@ -489,3 +489,7 @@ function repeatStr(n, s) {
 function correctTail(bod, tail) {
     return bod[bod.length - 1] === tail
 }
+
+function validateUsr(username) {
+    return /^([a-z0-9_]){4,16}$/.test(username)
+  }
